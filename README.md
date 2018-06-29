a94e4b93aa2cdc6b414449731b6a941a7e017ad2
Jared Kranzler <kranzler312@gmail.com>
2018-06-29 16:56:54 -0500

    BattleShip begin

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # GA_Project1_BattleShips
+By Jared Kranzler
+
+Take turns firing upon the enemy ship by marking plots on the grid.
+
+Hits will show up as an ("x") & misses as ("o").
+
+First to sink all the others ships wins!
+
+
+
+MVP
+•create game board
+•create logic
+•One Player can do the following
+•Start game
+•Select where to place ship on board
+•See hits and misses with colors
+•See if ship has been hit
+•Restart game
+
+MVP2
+•Two Player
+
+MVP3
+•Player vs CPU
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/app.js
@@ -0,0 +1,93 @@
+console.log("You Sunk My BattleShip!!!")
+
+//object for holding playerfleet info
+
+// class Fleet {
+//   constructor(ships, shipsLength, ){
+//   // store ships in an array
+//     this.ships = []
+//     //hold length left of ship
+//     this.shipsLength = [5, 4, 3, 3, 2]
+//     //store ship location
+//     this.shipLocation = shipLocation;
+//     //store hits
+//     this.hit =  hit;
+//     //stor missed
+//     this.miss = miss;
+//     //Game board
+//     this.gameBoard = 
+//   }
+// //mathod to place ships
+//   startGame(){
+
+//   }
+//   // method for slecting grid items
+//   click(){
+
+//   }
+//     // method to locate ships when clicking for placement
+//   findShip(){
+
+//   }
+//   //method to place ships in begining
+//   placeShip (){
+
+//   }
+//   //method to execute attack
+//   shoot(){
+
+//   }
+//   //method to mark hits and misses on the board
+//   hitOrMiss(){
+
+//   }
+//   // method to store remaining length after turn
+//   storeLength(){
+
+//   }
+//   //method to update game board after turn
+//   updateBoard(){
+
+//   }
+//   //method to switch turns
+//   changeTurn(){
+
+//   }
+//   //method for sinking ships
+//   sinkShip() {
+
+//   }
+// }
+
+// const player = new Fleet(ships, shipsLength, shipLocation)
+
+// const cpu = new Fleet(ships, shipsLength, shipLocation)
+
+
+// make the graph out of divs in html
+
+// maybe make a loop also for clicking on grid
+
+// right rules that dont allow putting ships length 
+// out of strait line
+
+// rule that changes turn each shot
+
+// rule that hits or misses
+
+//rule that sinks ship
+
+//need to figure out how to make computers turn random
+//while placing ships in an ordered line still 
+
+
+
+
+
+
+
+
+
+
+
+
